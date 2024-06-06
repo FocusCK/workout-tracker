addca46403b187b245c0436fd16e41572a728ece
colm kelleher <ckcolmkelleher@gmail.com>
2024-06-06 13:33:26 +0100

    Updated ids and variable names to allow for a weights tracker

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-document.getElementById('form').addEventListener('submit', addWorkout);
-function addWorkout(event) {
+document.getElementById('cardio-form').addEventListener('submit', addCardio);
+function addCardio(event) {
     event.preventDefault();
     const date = document.getElementById('date').value;
     const exercise = document.getElementById('exercise').valuel;
     const duration = document.getElementById('duration').value;
-    const workout = { date, exercise, duration };
-    let workouts = JSON.parse(localStorage.getItem('workouts'));
-    workouts.push(workouts);
-    localStorage.setItem('workouts', JSON.stringify(workouts));
+    const cardio = { date, exercise, duration };
+    let cardios = JSON.parse(localStorage.getItem('cardio'));
+    cardios.push(cardio);
+    localStorage.setItem('cardio', JSON.stringify(cardio));
 }
