8ddfd05b8cdf6f362eda3fe29e45bce0eb94170b
colm kelleher <ckcolmkelleher@gmail.com>
2024-06-06 17:50:27 +0100

    Fixed form to suit weight-lifting

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
-document.getElementById('cardio-form').addEventListener('submit', addCardio);
-function addCardio(event) {
+document.getElementById('workout-form').addEventListener('submit', addWorkout);
+
+function addWorkout(event) {
     event.preventDefault();
     const date = document.getElementById('date').value;
     const exercise = document.getElementById('exercise').valuel;
-    const duration = document.getElementById('duration').value;
-    const cardio = { date, exercise, duration };
-    let cardios = JSON.parse(localStorage.getItem('cardio'));
-    cardios.push(cardio);
-    localStorage.setItem('cardio', JSON.stringify(cardio));
+    const weight = document.getElementById('weight').value;
+    const reps = document.getElementById('reps');
+    const sets = document.getElementById('sets');
+
+    const workout = { date, exercise, weight, reps, sets };
+
+    let workouts = JSON.parse(localStorage.getItem('workout'));
+    workouts.push(workout);
+    localStorage.setItem('cardio', JSON.stringify(workout));
 }
