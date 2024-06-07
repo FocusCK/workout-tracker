a8a423e5e9f159e6cc651100fdbc2dc5eb7d0ded
colm kelleher <ckcolmkelleher@gmail.com>
2024-06-07 22:26:10 +0100

    Add data to end of table

--- app.js
+++ app.js
@@ -14,20 +14,44 @@ function addWorkout(event) {
     workouts.push(workout);
     localStorage.setItem('workouts', JSON.stringify(workouts));
 
-    displayWorkouts();
+    // displayWorkouts();
+    addWorkoutToTable(workout);
+    document.getElementById('workout-form').requestFullscreen();
 }
 
+// function displayWorkouts() {
+//     const workouts = JSON.parse(localStorage.getItem('workouts')) || [];
+//     const workoutList = document.getElementById('workout-list');
+//     workoutList.innerHTML = workouts.map(workout => `
+//         <tr>
+//         <td>${workout.date}</td>
+//         <td>${workout.exercise}</td>
+//         <td>${workout.weight}</td>
+//         <td>${workout.reps}</td>
+//         <td>${workout.sets}</td>
+//         </tr>`).join('');
+// }
+// displayWorkouts();
+
+
+function addWorkoutToTable(workout) {
+    const workoutList = document.getElementById('workout-list');
+    const row = document.createElement('tr');
+
+    row.innerHTML = `<td>${workout.date}</td> <td>${workout.exercise}</td> <td>${workout.weight}</td> <td>${workout.reps}</td> <td>${workout.sets}</td>`;
+    if (workoutList.firstChild) {
+        workoutList.insertBefore(row, workoutList.firstChild);
+    } else {
+        workoutList.appendChild(row);
+    }
+}
 function displayWorkouts() {
     const workouts = JSON.parse(localStorage.getItem('workouts')) || [];
     const workoutList = document.getElementById('workout-list');
-    workoutList.innerHTML = workouts.map(workout => `
-        <tr>
-        <td>${workout.date}</td>
-        <td>${workout.exercise}</td>
-        <td>${workout.weight}</td>
-        <td>${workout.reps}</td>
-        <td>${workout.sets}</td>
-        </tr>`).join('');
+    workoutList.innerHTML = '';
+    
+    workouts.forEach(workout => {
+        addWorkoutToTable(workout);
+    });
 }
 displayWorkouts();
-
